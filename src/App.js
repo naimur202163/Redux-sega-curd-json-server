612d2e4b9d118b6126a2e79ced6a428e1f4c5276
Your Name~ <naiumur202163@gmail.com>
2022-02-17 15:08:33 +0600

    Router added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-
+import About from './pages/about';
+import AddEditUser from './pages/AddEditUser';
+import Home from './pages/Home';
+import UserInfo from './pages/UserInfo';
 function App() {
   return (
     <div className="App">
-          <h2>Naimur Rahman</h2>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route exact path="/addUser" component={AddEditUser}></Route>
+          <Route exact path="/editUser/:id" component={AddEditUser}></Route>
+          <Route exact path="/userInfo" component={UserInfo}></Route>
+          <Route exact path="/about" component={About}></Route>
+        </Switch>
+      </BrowserRouter>
+          <h2 className='fb-bold'>Naimur Rahman</h2>
     </div>
   );
 }
