58edd77f2f8a7920555439fac6a6f0e2278d35ff
Your Name~ <naiumur202163@gmail.com>
2022-02-17 13:14:42 +0600

    this is my frist web-site

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
